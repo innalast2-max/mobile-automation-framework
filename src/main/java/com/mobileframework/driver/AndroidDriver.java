15db3350c43ea93d887f1035c6409fecc40b1917
Inna Yukhymenko <inna.yukhymenko@epam.com>
2026-08-05 23:37:30 +0300

    Real AndroidDriver via UiAutomator2, config for MyDemo app, smoke passes on emulator

--- src/main/java/com/mobileframework/driver/AndroidDriver.java
+++ src/main/java/com/mobileframework/driver/AndroidDriver.java
@@ -1,20 +1,38 @@
 package com.mobileframework.driver;
 
+import com.mobileframework.config.ConfigLoader;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.time.Duration;
 
 public class AndroidDriver implements Driver {
     private io.appium.java_client.android.AndroidDriver appiumDriver;
 
     @Override
     public void start() {
-
-        throw new UnsupportedOperationException(
-                "Android driver is not implemented yet. Use platform=IOS.");
+        var config = ConfigLoader.getInstance();
+        var options = new UiAutomator2Options()
+                .setAppPackage(config.getRequiredProperty("android.appPackage"))
+                .setAppActivity(config.getRequiredProperty("android.appActivity"));
+        try {
+            appiumDriver = new io.appium.java_client.android.AndroidDriver(
+                    URI.create(config.getProperty("appium.server.url")).toURL(), options);
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException("Invalid Appium server URL", e);
+        }
+        // TODO: remove when explicit waits land in BasePage
+        appiumDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @Override
     public void stop() {
-        System.out.println("Android driver stopped");
+        if (appiumDriver != null) {
+            appiumDriver.quit();
+            appiumDriver = null;
+        }
     }
 
     @Override
@@ -23,5 +41,7 @@ public class AndroidDriver implements Driver {
     }
 
     @Override
-    public AppiumDriver getAppiumDriver() { return appiumDriver; }
+    public AppiumDriver getAppiumDriver() {
+        return appiumDriver;
+    }
 }
