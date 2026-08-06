960c16f3f47348af068c52ab38422a18af974398
Inna Yukhymenko <inna.yukhymenko@epam.com>
2026-08-06 10:48:18 +0300

    Extract shared driver logic into Driver interface: static serverUrl() and default stop()

--- src/main/java/com/mobileframework/driver/AndroidDriver.java
+++ src/main/java/com/mobileframework/driver/AndroidDriver.java
@@ -4,8 +4,6 @@ import com.mobileframework.config.ConfigLoader;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.time.Duration;
 
 public class AndroidDriver implements Driver {
@@ -17,22 +15,9 @@ public class AndroidDriver implements Driver {
         var options = new UiAutomator2Options()
                 .setAppPackage(config.getRequiredProperty("android.appPackage"))
                 .setAppActivity(config.getRequiredProperty("android.appActivity"));
-        try {
-            appiumDriver = new io.appium.java_client.android.AndroidDriver(
-                    URI.create(config.getProperty("appium.server.url")).toURL(), options);
-        } catch (MalformedURLException e) {
-            throw new IllegalStateException("Invalid Appium server URL", e);
-        }
+        appiumDriver = new io.appium.java_client.android.AndroidDriver(Driver.serverUrl(), options);
         // TODO: remove when explicit waits land in BasePage
         appiumDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-
-    @Override
-    public void stop() {
-        if (appiumDriver != null) {
-            appiumDriver.quit();
-            appiumDriver = null;
-        }
     }
 
     @Override
